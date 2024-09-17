db7d997ac1cb30c55622f8fc6a7ca0edf724d8d8
KirichokEV <kirichok.liza@bk.ru>
2024-09-17 14:21:02 +0500

    Task3

---
+++ Tyuiu.KirichokEV.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.KirichokEV.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //Реализация метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
