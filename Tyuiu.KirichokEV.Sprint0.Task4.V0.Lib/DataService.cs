d37cb2307e29ea37d7cf32d3d1fb5f61b556dc56
KirichokEV <kirichok.liza@bk.ru>
2024-09-17 17:50:11 +0500

    Task4

---
+++ Tyuiu.KirichokEV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿namespace Tyuiu.KirichokEV.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной функции
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структруры 
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры 
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        //Пример линейной структуры 
+        public static int Division(int a, int b)
+        {
+            return a / b; 
+        }
+    }
+}
