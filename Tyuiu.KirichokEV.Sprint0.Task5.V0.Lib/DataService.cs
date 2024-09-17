987d423cda62cd769112d1ecb0ae289dde03aa46
KirichokEV <kirichok.liza@bk.ru>
2024-09-17 19:01:01 +0500

    Task5

---
+++ Tyuiu.KirichokEV.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿namespace Tyuiu.KirichokEV.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной функции
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структруры 
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры 
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        //Пример линейной структуры 
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                //Пример создан в целях демонстрации ветвления
+                //В реальных проектах нужно использовать Exeption
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+    }
+}
