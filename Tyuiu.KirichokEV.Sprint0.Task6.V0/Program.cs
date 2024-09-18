64de61119ab8565352eb052d63b42b8de2420813
KirichokEV <kirichok.liza@bk.ru>
2024-09-18 18:37:30 +0500

    Task6

---
+++ Tyuiu.KirichokEV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KirichokEV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KirichokEV.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            //Пример циклической структуры находится в библиотеки классов методе AdditioarrayArray
+            Console.WriteLine(" Сумма элементо массива = " + DataService.AdditionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеки классов методе SubtractionArray
+            Console.WriteLine(" Разность элементо массива = " + DataService.SubtractionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеки классов методе MultiplicationArray
+            Console.WriteLine(" Произведение элементо массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
